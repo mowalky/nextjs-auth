0efce508f2153b4be403d886601fa4e3cee255ae
Mowalky <mowalky@outlook.com>
2022-06-08 14:11:22 -0500

    login implementation

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,6 @@
 import { useState, useRef } from "react";
+import { signIn } from "next-auth/client";
+
 import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
@@ -36,6 +38,12 @@ function AuthForm() {
     event.preventDefault();
 
     if (isLogin) {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: emailInputRef.current.value,
+        password: passwordInputRef.current.value,
+      });
+      console.log(result);
     } else {
       try {
         const result = await createUser(
