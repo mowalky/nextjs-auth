3957e5401e12397768b897e7739eb057649a10ab
Mowalky <mowalky@outlook.com>
2022-06-08 14:32:17 -0500

    page guard

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,29 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { getSession } from "next-auth/client";
+import { useState, useEffect } from "react";
+
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const [isLoading, setLoading] = useState(true);
+  const [loadedSession, setLoadedSession] = useState();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  useState;
+
+  if (isLoading) {
+    return <p className={classes.profile}>LOading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
